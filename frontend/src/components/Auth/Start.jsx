6bdd40d998088f722d6cd5f68951eb82266daf55
Vimala2020 <vimala.rajkumar92@gmail.com>
2024-07-04 10:28:24 +0530

    start(employee or login)

---
+++ frontend/src/components/Auth/Start.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Start() {
+  return (
+    <div className='d-flex justify-content-center align-items-center vh-100'>
+        <div className='p-3 rounded w-24 border'>
+            <h2 className=' text-center'> Login As</h2>
+
+        </div>
+
+    </div>>
+  )
+}
+
+export default Start
