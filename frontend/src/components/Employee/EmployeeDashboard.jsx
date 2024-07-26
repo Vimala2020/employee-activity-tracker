3231b66159f71d8ee6d81b1c00d605719e545d20
Vimala2020 <vimala.rajkumar92@gmail.com>
2024-07-26 13:26:10 +0530

    employee  dash updated

--- frontend/src/components/Employee/EmployeeDashboard.jsx
+++ frontend/src/components/Employee/EmployeeDashboard.jsx
@@ -18,40 +18,34 @@ const EmployeeDashboard = () => {
 
   useEffect(() => {
     const auth = getAuth();
-    const currentUser = auth.currentUser;
+    const unsubscribe = auth.onAuthStateChanged(currentUser => {
+      if (currentUser) {
+        const userInfo = {
+          email: currentUser.email,
+          uid: currentUser.uid,
+        };
 
-    if (currentUser) {
-      const userInfo = {
-        email: currentUser.email,
-        uid: currentUser.uid,
-      };
+        localStorage.setItem('user', JSON.stringify(userInfo));
+        setUser(userInfo);
+      } else {
+        localStorage.removeItem('user');
+        setUser({ email: '' });
+      }
+    });
 
-      localStorage.setItem('user', JSON.stringify(userInfo));
-      setUser(userInfo);
-    } else {
-      localStorage.removeItem('user');
-    }
+    return () => unsubscribe();
   }, []);
 
-  useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem('user'));
-    if (storedUser) {
-      setUser(storedUser);
-    }
-  }, []);
+  
 
   return (
     <div className="flex h-screen overflow-hidden">
       <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <div className="flex flex-1 flex-col">
+      <div className="flex flex-1 flex-col main-container">
         <Header user={user} toggleSidebar={toggleSidebar} onLogout={Logout} />
-        <main className="flex-1 overflow-y-auto p-4 mt-16">
-          <h1 className="text-3xl font-bold mb-4">Welcome, {user.email}</h1>
-          <div className="flex flex-col lg:flex-row gap-4">
-            <div className="bg-white p-6 rounded-lg shadow-md w-full lg:w-1/2">
-              <p><strong>Email:</strong> {user.email}</p>
-            </div>
-          </div>
+        <main className="flex flex-1 flex-col justify-center items-center p-4 mt-16">
+          <h1 className="text-3xl font-bold mb-10">Welcome </h1>
+          <h5>{user.email}</h5>
           <Routes>
             <Route path="attendance/*" element={<AttendanceForm />} />
             <Route path="work-progress/*" element={<WorkprogressForm />} />
